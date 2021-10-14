e6351c70aebc2bb04ebc8c5eb7b311c24b20755b
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-10-14 09:47:34 +0200

    remove traveler

--- server/src/server.js
+++ server/src/server.js
@@ -105,39 +105,6 @@ app.post("/flightCreateOrder", async function (req, res) {
           travelers: [
             {
               id: "1",
-              dateOfBirth: "1982-01-16",
-              name: {
-                firstName: "JORGE",
-                lastName: "GONZALES",
-              },
-              gender: "MALE",
-              contact: {
-                emailAddress: "jorge.gonzales833@telefonica.es",
-                phones: [
-                  {
-                    deviceType: "MOBILE",
-                    countryCallingCode: "34",
-                    number: "480080076",
-                  },
-                ],
-              },
-              documents: [
-                {
-                  documentType: "PASSPORT",
-                  birthPlace: "Madrid",
-                  issuanceLocation: "Madrid",
-                  issuanceDate: "2015-04-14",
-                  number: "00000000",
-                  expiryDate: "2025-04-14",
-                  issuanceCountry: "ES",
-                  validityCountry: "ES",
-                  nationality: "ES",
-                  holder: true,
-                },
-              ],
-            },
-            {
-              id: "2",
               dateOfBirth: "2012-10-11",
               gender: "FEMALE",
               contact: {
